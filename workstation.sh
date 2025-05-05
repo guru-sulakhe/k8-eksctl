4df2f83d176e3899c394cda536cb2ec9eb0feab8
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-05-05 15:50:02 +0530

    creating kubernetes cluster through eksctl commands

--- workstation.sh
+++ workstation.sh
@@ -36,14 +36,14 @@ systemctl enable docker
 usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
-# eksctl
+# eksctl --> eksctl is responsible for creating and managing kubenetes cluster of AWS
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 eksctl version
 VALIDATE $? "eksctl installation"
 
 
-# kubectl
+# kubectl --> kubectl is responsible for creating and managing containers inside eks cluster
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
